0e9215b74c70c99dbddae4267ac3b1e545f8dea4
Yuquan <37016889+kevincai14@users.noreply.github.com>
2024-09-19 23:52:45 +0800

    Revert "version3"
    This reverts commit 62fa4308

--- C++-CSC3100/Assignment 1/A1.cpp
+++ C++-CSC3100/Assignment 1/A1.cpp
@@ -34,14 +34,26 @@ void update(vector<int> parameter, vector<int> & array) {
 
 int check_distinct(vector<int> array) {
     unordered_set<int> distinct_set;
-    for (int i: array) {
-        auto a = distinct_set.insert(i);
-        if (a.second == false) {
-            distinct_set.insert(-i);
-        }
+    for (int i : array) {
+        distinct_set.insert(i);
     }
     return distinct_set.size();
 }
+
+//int check_distinct(vector<int> array) {
+//    int distinct = 0;
+//    vector<int> distinct_array;
+//    for (int i : array) {
+//        if (find(distinct_array.begin(), distinct_array.end(), i) == distinct_array.end()) {
+//            distinct_array.push_back(i);
+//            distinct++;
+//        } else if (find(distinct_array.begin(), distinct_array.end(), -i) == distinct_array.end()) {
+//            distinct_array.push_back(-i);
+//            distinct++;
+//        }
+//    }
+//    return distinct;
+//}
 
 void operate(string command, vector<int> &array) {
     if (command == "2") {
@@ -63,33 +75,20 @@ void operate(string command, vector<int> &array) {
 int main() {
     vector<int> array;
     vector<string> source_v;
-    int status = 0;
-//    int m;
+    int m;
     string line;
 
     while (cin) {
         getline(cin, line);
-        if (status == 0) {
-//            m = get_array((line))[1];
-            P = get_array(line)[2];
-            status++;
-        } else if (status == 1) {
-            array = get_array(line);
-        } else {
-            operate(line, array);
-        }
+        source_v.push_back(line);
     }
 
-//    m = get_array((source_v[0]))[1];
-//    P = get_array(source_v[0])[2];
+    m = get_array((source_v[0]))[1];
+    P = get_array(source_v[0])[2];
 
-//    array = get_array(source_v[1]);
-//    for (int i = 0; i < m; i++) {
-//        operate(source_v[i + 2], array);
-//        for (int i : array) {
-//            cout << i << " ";
-//        }
-//        cout << endl;
-//    }
+    array = get_array(source_v[1]);
+    for (int i = 0; i < m; i++) {
+        operate(source_v[i + 2], array);
+    }
     system("pause");
 }

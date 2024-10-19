c54f1e3b5f517ee5c958e31b28166a902e1fc0b4
Yuquan <37016889+kevincai14@users.noreply.github.com>
2024-10-19 21:22:24 +0800

    q2 v1
    success (need improve)

--- C++-CSC3100/Assignment 2/A2_P2_122090007.cpp
+++ C++-CSC3100/Assignment 2/A2_P2_122090007.cpp
@@ -4,35 +4,70 @@
 #include <iostream>
 #include <cctype>
 #include <vector>
+#include <stack>
+#include <algorithm>
 using namespace std;
 
 int n;
-int e_count = 0;
 
-int check_loop(int L, int complexity = 0, int end_count = 0) {
-    string F, i, x, y;
-    cin >> F;
-
-    if (F == "F") {
-        cin >> i >> x >> y;
-
-        bool xy_state = isdigit(x[0]) and isdigit(y[0]);
-
-        if (x != y and !xy_state and e_count != 1) {
-            complexity++;
+int max_f(vector<int> complexity_list, int index) {
+    if (index + 1 == complexity_list.size()) {
+        return 0;
+    } else {
+        int max_num = 0;
+        for (int i = index + 1; i < complexity_list.size(); i++) {
+            if (complexity_list[i] > max_num) {
+                max_num = complexity_list[i];
+            }
         }
-        e_count = 0;
+        return max_num;
+    }
+}
 
-        return check_loop(L, complexity, end_count);
+void fill0(vector<int> & complexity_list, int index) {
+    if (index + 1 == complexity_list.size()) {
+        complexity_list = complexity_list;                      //fixxxxxxxxxxxxxxxxxx
+    } else {
+        for (int i = index + 1; i < complexity_list.size(); i++) {
+            complexity_list[i] = 0;
+        }
+    }
+}
 
-    } else if (F == "E") {
-        e_count++;
-        end_count++;
-        if (end_count == L / 2) {
-            e_count = 0;
-            return complexity;
-        } else {
-            return check_loop(L, complexity, end_count);
+int check_loop(int L) {
+    string F, i, x, y;
+    int end_count = 0;
+    int F_count = 0;
+    vector<int> complexity_list;
+    stack<int> F_index;
+
+    while (true) {
+        cin >> F;
+        if (F == "F") {
+            cin >> i >> x >> y;
+            F_index.push(F_count);
+            F_count++;                   //combine with above F_index.push(F_count++)
+            bool xy_state = isdigit(x[0]) and isdigit(y[0]);
+
+            if (x != y and !xy_state) {
+                complexity_list.push_back(1);
+            } else {
+                complexity_list.push_back(0);
+            }
+
+        } else if (F == "E") {
+            end_count++;
+
+            int index = F_index.top();
+            int max_num = max_f(complexity_list, index);
+            complexity_list[index] += max_num;
+            fill0(complexity_list, index);
+
+            F_index.pop();
+
+            if (end_count == L / 2) {
+                return complexity_list[0];
+            }
         }
     }
 
--- C++-CSC3100/draft/4.cpp
+++ C++-CSC3100/draft/4.cpp
@@ -2,5 +2,40 @@
 // Created by Quan on 2024/10/19.
 //
 #include <iostream>
-#include <cctype>
+#include <vector>
+#include <algorithm> // std::max_element
+
 using namespace std;
+int max_f(vector<int> complexity_list, int index) {
+    if (index + 1 == complexity_list.size()) {
+        return 0;
+    } else {
+        int max_num = 0;
+        for (int i = index + 1; i < complexity_list.size(); i++) {
+            if (complexity_list[i] > max_num) {
+                max_num = complexity_list[i];
+            }
+        }
+        return max_num;
+    }
+}
+int main() {
+    vector<int> complexity_list = {1,9,7,1};
+    int index = 0;
+
+
+//    auto max_num = max_element(complexity_list.begin() + index + 1, complexity_list.end());
+
+    int max_num = max_f(complexity_list, index);
+    complexity_list[index] += max_num;
+        cout << "maxnum" << max_num << endl;
+    fill(complexity_list.begin() + index + 1, complexity_list.end(), 0);
+
+
+    for (int value : complexity_list) {
+        cout << value << " ";
+    }
+    cout << endl;
+    return 0;
+}
+

9290295b9dc27c4c7efc7ea5927c58a88906d511
Yuquan <37016889+kevincai14@users.noreply.github.com>
2024-12-07 02:22:16 +0800

    update
    using array

--- C++-CSC3100/Assignment 4/A4_P2_122090007.cpp
+++ C++-CSC3100/Assignment 4/A4_P2_122090007.cpp
@@ -9,53 +9,46 @@ using namespace std;
 
 int n, m, q;
 
-void dfs(int s, int t, vector<vector<int>>& adjacency_matrix, vector<int>& current_path, vector<bool>& visited, vector<vector<int>>& all_path) {
+void dfs(int s, int t, vector<vector<pair<int, int>>>& adjacency_list, vector<int>& current_path, vector<bool>& visited, vector<vector<int>>& all_path) {
     current_path.push_back(s);
     visited[s] = true;
 
     if (s == t) {
         all_path.push_back(current_path);
     } else {
-        for (int neighbor = 0; neighbor < n; neighbor++) {
-            if (adjacency_matrix[s][neighbor] != INT32_MAX and !visited[neighbor]) {
-                dfs(neighbor, t, adjacency_matrix, current_path, visited, all_path);
+        for (auto& neighbor : adjacency_list[s]) {
+            int neighbor_node = neighbor.first;
+            if (!visited[neighbor_node]) {
+                dfs(neighbor_node, t, adjacency_list, current_path, visited, all_path);
             }
         }
     }
+
     current_path.pop_back();
     visited[s] = false;
 }
 
-int max_min_path(int s, int t, vector<vector<int>>& adjacency_matrix) {
+int max_min_path(int s, int t, vector<vector<pair<int, int>>>& adjacency_list) {
     vector<int> current_path;
     vector<bool> visited(n, false);
     vector<vector<int>> all_path;
-    dfs(s, t, adjacency_matrix, current_path, visited, all_path);
-//    for (auto i: all_path) {
-//        for (auto j :i) {
-//            cout << j+1 << " ";
-//        }
-//        cout << "end" <<endl;
-//    }
-//    for (auto i:adjacency_matrix) {
-//        for (auto j: i) {
-//            if (j == INT32_MAX) {
-//                cout << "q" << " ";
-//            } else {
-//                cout << j << " ";
-//            }
-//        }
-//        cout << endl;
-//    }
-//    cout << endl;
+    dfs(s, t, adjacency_list, current_path, visited, all_path);
 
     int temp_max = 0;
     for (auto path: all_path) {
         int temp_min = INT32_MAX;
-        for (int j = 0; j < path.size(); j++) {
-            if (j + 1 < path.size()) {
-                temp_min = min(temp_min, adjacency_matrix[path[j]][path[j + 1]]);
+        for (int j = 0; j < path.size() - 1; j++) {
+            int u = path[j];
+            int v = path[j + 1];
+
+            int edge_weight = INT32_MAX;
+            for (auto& neighbor : adjacency_list[u]) {
+                if (neighbor.first == v) {
+                    edge_weight = neighbor.second;
+                    break;
+                }
             }
+            temp_min = min(edge_weight, temp_min);
         }
         temp_max = max(temp_max, temp_min);
     }
@@ -68,20 +61,14 @@ int main() {
         cout << 0;
         return 0;
     }
-    vector<vector<int>> adjacency_matrix(n, vector<int>(n, INT32_MAX));
 
+    vector<vector<pair<int, int>>> adjacency_list(n);
     for (int i = 0; i < m; i++) {
         int u, v, w;
         cin >> u >> v >> w;
-        adjacency_matrix[u - 1][v - 1] = w;
-        adjacency_matrix[v - 1][u - 1] = w;
+        adjacency_list[u - 1].push_back({v - 1, w});
+        adjacency_list[v - 1].push_back({u - 1, w});
     }
-//    for (auto i: adjacency_matrix) {
-//        for (auto j: i) {
-//            cout << j << " ";
-//        }
-//        cout << endl;
-//    }
 
     cin >> q;
     vector<vector<tuple<int, int, int>>> change_edge;
@@ -107,27 +94,31 @@ int main() {
         cin >> s >> t;
         start_end.push_back({s, t});
     }
-//    for (auto i: start_end) {
-//        cout << i[0] << i[1];
-//        cout << endl;
-//    }
 
     vector<int> result;
     for (int i = 0; i < q + 1; i++) {
         int s = start_end[i][0];
-
         int t = start_end[i][1];
+
         if (i != 0) {
             for (auto j: change_edge[i - 1]) {
                 int u = get<0>(j);
                 int v = get<1>(j);
                 int w = get<2>(j);
-//                cout << w << endl;
-//                cout << endl;
-                if (u <= n and u>=1 and v >= 1 and v <= n and w >=0) {
-                    if (adjacency_matrix[u - 1][v - 1] != INT32_MAX) {
-                        adjacency_matrix[u - 1][v - 1] = w;
-                        adjacency_matrix[v - 1][u - 1] = w;
+                if (u >= 1 and u <= n and v >= 1 and v <= n and w >= 0) {
+                    u--;
+                    v--;
+                    for (auto& neighbor : adjacency_list[u]) {
+                        if (neighbor.first == v) {
+                            neighbor.second = w;
+                            break;
+                        }
+                    }
+                    for (auto& neighbor : adjacency_list[v]) {
+                        if (neighbor.first == u) {
+                            neighbor.second = w;
+                            break;
+                        }
                     }
                 }
             }
@@ -143,7 +134,7 @@ int main() {
 //            cout << endl;
 //        }
 //        cout << endl;
-        int max_min_cost = max_min_path(s - 1, t - 1, adjacency_matrix);
+        int max_min_cost = max_min_path(s - 1, t - 1, adjacency_list);
 //        int max_min_cost = 0;
         result.push_back(max_min_cost);
     }
